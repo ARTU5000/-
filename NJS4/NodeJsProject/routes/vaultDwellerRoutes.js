c6fe54e3af72ad8b3bca8019ad52183758e24239
ARTU5000 <126347072+ARTU5000@users.noreply.github.com>
2024-05-29 08:34:48 -0600

    jaidjaiji

---
+++ NJS4/NodeJsProject/routes/vaultDwellerRoutes.js
@@ -0,0 +1,7 @@
+const vaultDwellerController = require('../controllers/vaultDwellerController');
+
+module.exports = (app) => {
+    app.post ('/api/vaultdweller/create', vaultDwellerController.register);
+    app.post ('/api/vaultdweller/getDweller', vaultDwellerController.getDweller);
+    app.get ('/api/vaultdweller/getAllDwellers', vaultDwellerController.getAllDwellers);
+}
